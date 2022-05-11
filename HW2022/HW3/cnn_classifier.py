0619e015b941ba3183d5dd1965c45ff6e4508cb1
GenadyKogan <rgkogan@gmail.com>
2022-05-11 16:09:22 +0300

    get_model changed

--- HW2022/HW3/cnn_classifier.py
+++ HW2022/HW3/cnn_classifier.py
@@ -26,20 +26,19 @@ class CNN_Classifier_HHD():
         # ----------------------- 1 preprocessing
         self.df_data_image = self.preprocessing(folder)
         
-        #print(self.df_data_image)
-        # ----------------------- 1 spliting data
+        # ----------------------- 2 spliting data
         self.all_train, self.all_val, self.all_test = self.split_dataset()
-        #print(self.all_train.shape, self.all_val.shape, self.all_test.shape)
+
         self.x_train, self.y_train = self.dataset_to_npArray_and_OneHotEncoder(self.all_train)
         self.x_val, self.y_val = self.dataset_to_npArray_and_OneHotEncoder(self.all_val)
         self.x_test, self.y_test = self.dataset_to_npArray_and_OneHotEncoder(self.all_test)
-        #print( self.x_train.shape,  self.x_val.shape,  self.x_test.shape)
-        #print( self.y_train.shape,  self.y_val.shape,  self.y_test.shape)
-        # ----------------------- 2 training
+
+        # ----------------------- 3 training
         #os.mkdir('data')
         # Get a configuration type of the model: 0 - configuration without augmentation, 1 - configuration with augmentation
         name_history, name_model = self.get_configuration_type_and_fit_model(0)
-        # ----------------------- 3 evaluation cnn
+
+        # ----------------------- 4 evaluation cnn
         # Load model
         model, history = self.load_models(name_history, name_model)
         self.plot_loss_and_accurecy_model(history, 'accuracy', 'Accuracy_'+name_model)
@@ -58,11 +57,14 @@ class CNN_Classifier_HHD():
         """
         h, w = img.shape[:2]
         sh, sw = (max(h, w), max(h, w))
+
         # interpolation method
         if h > sh or w > sw:  # shrinking image
             interp = cv2.INTER_AREA
+
         else:  # stretching image
             interp = cv2.INTER_CUBIC
+
         # aspect ratio of image
         aspect, saspect = float(w) / h, float(sw) / sh
 
@@ -107,15 +109,21 @@ class CNN_Classifier_HHD():
         df_data_image = pd.DataFrame(columns=['img_data', 'img_labels', 'dir_path'])
         dir_path = glob(folder + "/**/*.png", recursive=True)
         for img in dir_path:
+
             # Load image
             input_img = cv2.imread(img)
+
             # a- Convert image to grayscale
             input_img_cvtColor = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
+
             # b. Add white padding to the image so that its size is square
             color = [255, 255, 255]  # white color
+
             dst = self.add_white_padding(input_img_cvtColor, color)
+
             # c- Resize the image to a uniform size (32,32)
             input_img_resize = cv2.resize(dst, (32, 32))
+
             # d- Turning an image into a negative
             input_img_negative = 255 - input_img_resize
 
@@ -145,15 +153,20 @@ class CNN_Classifier_HHD():
         # Defining new data frames for all train, val, test
         all_train = pd.DataFrame()
         all_val, all_test = all_train, all_train
+
         # A loop that runs on all types of labels (0-26) and divides each label to 80% train, 10% val and 10% test
         for label in set(self.df_data_image['img_labels']):
+
             # Take 80% of the data on a particular label to train
             train = self.df_data_image[self.df_data_image['img_labels'] == label].sample(frac=0.8, random_state = 42)
+            
             # not_train - Contains 20% data that are not train
             not_train = self.df_data_image[self.df_data_image['img_labels'] == label].drop(train.index)
+
             # Of the remaining 20%, half go to val and half to test
             val = not_train.sample(frac=0.5, random_state = 42)
             test = not_train.drop(val.index)
+
             # Append all the data to dataframes
             all_train, all_val, all_test = all_train.append(train), all_val.append(val), all_test.append(test)
         print("The division of the data into train, validation and test was successful!")
@@ -167,11 +180,11 @@ class CNN_Classifier_HHD():
                  y - Image label (0-26 classification) after conversion to np array and one hot.
         """
         # Convert a dataset images and labels to np.array
-        X = np.array(df['img_data'].values.tolist())
+        x = np.array(df['img_data'].values.tolist())
         y = np.array(df['img_labels'].values.tolist())
         #  Transform labels to one hot encoding
         y = tf.keras.utils.to_categorical(y, num_classes=len(set(df['img_labels'])))
-        return X, y
+        return x, y
     #------------------------------------------------------------------------------ 2 training
     def save_model(self, history, model, name_history, name_model):
         np.save('data/'+ name_history + '.npy', history.history)
@@ -209,32 +222,67 @@ class CNN_Classifier_HHD():
         '''
         # Define Sequential model
         model = keras.Sequential()
+    
         # First iteration: CONV=>RELU=>CONV=>RELU=>POOL=>DO
         model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same', input_shape=(32, 32, 1)))
         model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.25))
+
         # Second iteration: CONV=>RELU=>CONV=>RELU=>POOL=>DO
         model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same'))
         model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.25))
+
         # Third iteration: CONV=>RELU=>CONV=>RELU=>POOL=>DO
         model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same'))
         model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.25))
+
         # Layer FC => RELU => DO
         model.add(Flatten())
         model.add(Dense(512, activation='relu'))
         model.add(Dropout(0.5))
+
         # The last FC layer with 27 neurons
         model.add(Dense(27, activation='softmax'))
 
         # config the model with losses and metrics with model.compile()
         model.compile(loss=tf.keras.losses.CategoricalCrossentropy(), optimizer="Adam", metrics=["accuracy"])
         print(model.summary())
+
+        """
+        model = Sequential()
+
+        model.add(Conv2D(32, (3, 3), padding="same", input_shape=(32, 32, 1)))
+
+        model.add(Conv2D(32, (3, 3), padding="same", activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(Conv2D(64, (3, 3), padding="same",activation='relu'))
+        model.add(Conv2D(64, (3, 3), padding="same", activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(Conv2D(128, (3, 3), padding="same",activation='relu'))
+        model.add(Conv2D(128, (3, 3), padding="same", activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(Flatten())
+
+        model.add(Dense(512,activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(27,activation='softmax'))
+
+        model.summary()
+        """
         return model
+
+
 
     def data_augmentation(self):
         """
@@ -265,6 +313,7 @@ class CNN_Classifier_HHD():
         model = self.get_model()
         if choice == 0:  # Run configuration without augmentation
             name_history, name_model = 'hhd_cnn_history_without_augmentation', 'hhd_cnn_model_without_augmentation'
+
             # train the model with model.fit()
             history = model.fit(self.x_train, self.y_train, validation_data=(self.x_val, self.y_val), epochs=50, batch_size=256)
 
@@ -320,11 +369,13 @@ class CNN_Classifier_HHD():
 
         # Now the normalize the diagonal entries
         cm = con.astype('float') / con.sum(axis=1)[:, np.newaxis]
+
         # The diagonal entries are the accuracies of each class
         letter, accuracy = list(set(self.df_data_image['img_labels'])), cm.diagonal()
         accuracy_letter_dict = dict(zip(letter, accuracy))
         avg = str(sum(accuracy_letter_dict.values()) / len(accuracy_letter_dict.keys()))
         with open('results.txt', 'w') as file:
+
             # Write the data
             # ------------- a
             file.writelines("Configuration of the final model: " + str(name_configuration_final_model) + '\n\n')
@@ -346,8 +397,10 @@ class CNN_Classifier_HHD():
         y_test_list=[ int(np.argmax(y)) for y in self.y_test]
         con = confusion_matrix(y_test_list, pred_list)
         self.plot_confusion_matrix(con)
+
         # Printing precision,recall,accuracy score etc
         print(classification_report(y_test_list, pred_list))
+
         # Writing results into files
         self.write_to_result_txt(con, name_configuration_final_model)
         self.wrire_cm_to_csv(con)
@@ -356,6 +409,7 @@ class CNN_Classifier_HHD():
 if __name__ == "__main__":
 
     path = sys.argv[1]
+
     # Check If Path Exists
     if os.path.exists(path):
         print("path exists")
